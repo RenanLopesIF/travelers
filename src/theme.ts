352409ba733ead89c65852336b5393a5e9720dcb
Renan Lopes <renan.lopes@nextpage.com.br>
2022-08-19 12:16:51 -0300

    style: edit theme.tsx

--- src/theme.ts
+++ src/theme.ts
@@ -1,26 +1,47 @@
 import { extendTheme } from '@chakra-ui/react';
 
-const colors = {
-  primary: {
-    500: '#f0c',
+const theme = extendTheme({
+  colors: {
+    primary: {
+      50: '#dcf8ff',
+      100: '#b2e5fc',
+      200: '#86d3f6',
+      300: '#59c1f0',
+      400: '#2dafeb',
+      500: '#1496d2',
+      600: '#0574a4',
+      700: '#005376',
+      800: '#00334a',
+      900: '#00131d',
+    },
+    gray: {
+      50: '#f2f2f2',
+      100: '#d9d9d9',
+      200: '#bfbfbf',
+      300: '#a6a6a6',
+      400: '#8c8c8c',
+      500: '#737373',
+      600: '#595959',
+      700: '#404040',
+      800: '#262626',
+      900: '#0d0d0d',
+    },
+    black: {
+      50: 'rgba(0,0,0,0.04)',
+      100: 'rgba(0,0,0,0.08)',
+      200: 'rgba(0,0,0,0.12)',
+      300: 'rgba(0,0,0,0.15)',
+      400: 'rgba(0,0,0,0.3)',
+      500: 'rgba(0,0,0,0.45)',
+      600: 'rgba(0,0,0,0.6)',
+      700: 'rgba(0,0,0,0.75)',
+      800: 'rgba(0,0,0,0.9)',
+    },
   },
-  gray: {
-    50: '#f2f2f2',
-    100: '#dcd8d9',
-    200: '#bfbfbf',
+  config: {
+    // Changing initialColorMode to 'dark'
+    initialColorMode: 'dark',
   },
-  black: {
-    50: 'rgba(0,0,0,0.04)',
-    100: 'rgba(0,0,0,0.08)',
-    200: 'rgba(0,0,0,0.12)',
-    300: 'rgba(0,0,0,0.15)',
-    400: 'rgba(0,0,0,0.3)',
-    500: 'rgba(0,0,0,0.45)',
-    600: 'rgba(0,0,0,0.6)',
-    700: 'rgba(0,0,0,0.75)',
-    800: 'rgba(0,0,0,0.9)',
-  },
-};
+});
 
-const theme = extendTheme({ colors });
 export default theme;
