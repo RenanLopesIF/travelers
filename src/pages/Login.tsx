6e0b39fe27b51572e981714ddbad9b420aa2a584
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2022-08-26 12:18:37 -0300

    feat: add onSubmit logical into login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,52 @@
-import { Box, Button, Center, Image, Input, Link, Text, VStack } from '@chakra-ui/react';
-import React from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Button,
+  Center,
+  IconButton,
+  Image,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Link,
+  Spinner,
+  Text,
+  useToast,
+  VStack,
+} from '@chakra-ui/react';
+import React, { useRef, useState } from 'react';
 import Container from '../components/Container';
 
 function Login(): React.ReactElement {
+  const toast = useToast();
+  const inputUserRef = useRef<HTMLInputElement>(null);
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
+  const [isSubmiting, setIsSubmiting] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  function handleShowPassword() {
+    setShowPassword((prev) => !prev);
+  }
+
   function onSubmit() {
-    document.location = '/home';
+    setIsSubmiting(true);
+    toast.closeAll();
+    const username = inputUserRef.current?.value;
+    const password = inputPasswordRef.current?.value;
+    if (username === 'renanlopes' && password === 'renan123') {
+      document.location = '/home';
+    } else {
+      toast({
+        title: 'Usuário ou senha incorreto.',
+        variant: 'top-accent',
+        position: 'top',
+        isClosable: true,
+        status: 'warning',
+        duration: 1750,
+      });
+    }
+
+    setIsSubmiting(false);
   }
 
   return (
@@ -15,14 +57,52 @@ function Login(): React.ReactElement {
             <Image src="../../resources/logo.png" />
           </Box>
           <VStack spacing={4} flex={1} w="full" py={4}>
-            <Input colorScheme="primary" placeholder="Usuário" />
-            <Input colorScheme="primary" placeholder="Senha" />
+            <Input ref={inputUserRef} colorScheme="primary" placeholder="Usuário" />
+            <InputGroup size="md">
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                ref={inputPasswordRef}
+                colorScheme="primary"
+                placeholder="Senha"
+              />
+              <InputRightElement width="3.5em">
+                <IconButton
+                  variant="unstyled"
+                  onClick={handleShowPassword}
+                  aria-label={showPassword ? 'Mostrar senha' : 'Esconder senha'}
+                  icon={
+                    showPassword ? (
+                      <ViewIcon color="gray.700" />
+                    ) : (
+                      <ViewOffIcon color="gray.700" />
+                    )
+                  }
+                />
+              </InputRightElement>
+            </InputGroup>
           </VStack>
-          <Button colorScheme="primary" w="full" onClick={onSubmit}>
-            LOG IN
+          <Button
+            colorScheme="primary"
+            w="full"
+            onClick={onSubmit}
+            disabled={isSubmiting}
+            _disabled={{ opacity: 0.6, cursor: 'not-allowed' }}
+          >
+            LOGIN
+            {isSubmiting && <Spinner ml={3} />}
           </Button>
           <Text fontSize="sm" fontWeight={500}>
-            NÃO TEM CONTA? {<Link color="primary.600">CRIE UMA</Link>}
+            NÃO TEM CONTA?{' '}
+            {
+              <Link
+                color="primary.600"
+                href={!isSubmiting ? '/#' : undefined}
+                opacity={!isSubmiting ? 1 : 0.6}
+                cursor={!isSubmiting ? 'pointer' : 'not-allowed'}
+              >
+                CRIE UMA
+              </Link>
+            }
           </Text>
         </VStack>
       </Container>
