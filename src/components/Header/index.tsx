463c90bfd0f5babcd4101aa869c3f774915d077f
Renan Lopes <rfl5@aluno.ifnmg.edu.br>
2022-09-01 22:15:28 -0300

    feat: add notifications

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,6 +9,11 @@ import {
   Input,
   Avatar,
   Divider,
+  Menu,
+  MenuButton,
+  MenuItem,
+  Button,
+  MenuList,
 } from '@chakra-ui/react';
 import { SearchIcon, BellIcon, ChatIcon } from '@chakra-ui/icons';
 
@@ -36,8 +41,48 @@ function Header(): ReactElement {
         </InputGroup>
       </HStack>
       <HStack spacing="10px">
-        <ChatIcon width="25px" height="25px" color="black.700" />
-        <BellIcon width="25px" height="25px" color="black.700" />
+        <Menu>
+          <MenuButton
+            _active={{ backgroundColor: 'primary.300' }}
+            bgColor="transparent"
+            as={Button}
+          >
+            <BellIcon width="25px" height="25px" color="black.700" />
+          </MenuButton>
+          <MenuList>
+            <MenuItem minH="48px" borderBottom="1px" borderColor="gray.100">
+              <Image
+                boxSize="2rem"
+                borderRadius="full"
+                src="https://placekitten.com/100/100"
+                alt="Fluffybuns the destroyer"
+                mr="12px"
+              />
+              <span>Ana Ribeiro concorda com sua opinião</span>
+            </MenuItem>
+            <MenuItem minH="48px" borderBottom="1px" borderColor="gray.50">
+              <Image
+                boxSize="2rem"
+                borderRadius="full"
+                src="../../resources/partyplace/vitindopineu.jpg"
+                alt="Fluffybuns the destroyer"
+                mr="12px"
+              />
+              <span>Vitin do Pinei convidou você para um evento</span>
+            </MenuItem>
+            <MenuItem minH="48px" borderBottom="1px" borderColor="gray.100">
+              <Image
+                boxSize="2rem"
+                borderRadius="full"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1RrTgLueSZ0PEUk5WomywEc76OOZtJd-Rw&usqp=CAU"
+                alt="Simon the pensive"
+                mr="12px"
+              />
+              <span>André tem interesse em seu evento</span>
+            </MenuItem>
+          </MenuList>
+        </Menu>
+
         <Divider px="3px" orientation="vertical" />
         <Avatar size="sm" name="Profile" src="../../../resources/profile.webp" />
       </HStack>
